0c1f4d42c26b14eedd31d1fd799985cd530d75cb
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-08 00:37:22 +0530

    Added solution q1

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,58 +1,28 @@
  // TODO: Print a hollow diamond pattern of half-height n using '*'
+
+       package q01;
 import java.util.Scanner;
-public class HollowDiamond {
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter half height (n): ");
+        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
-        // Upper half
-        for (int i = 1; i <= n; i++) { 
-            // Spaces
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-            // Stars and hollow part
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) System.out.print(" ");
             for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
             }
             System.out.println();
         }
-        // Lower half
         for (int i = n - 1; i >= 1; i--) {
-            // Spaces
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            // Stars and hollow part
+            for (int j = n; j > i; j--) System.out.print(" ");
             for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
             }
             System.out.println();
         }
-        sc.close();
-    }
-}
-// Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
-
     }
 }
