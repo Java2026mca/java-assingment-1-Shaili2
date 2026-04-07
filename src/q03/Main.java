c1a430c0054ff912fa0050ad15be5ae7968f0204
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-07 23:46:22 +0530

    Added solution Q3

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,25 +1,38 @@
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        import java.util.Scanner;
-public class SpiralMatrix {
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter N: ");
+        
         int n = sc.nextInt();
+
+        // Input validation
+        if (n <= 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
         int[][] matrix = new int[n][n];
+
         int top = 0, bottom = n - 1;
         int left = 0, right = n - 1;
         int num = 1;
+
+        // Fill matrix in spiral order
         while (top <= bottom && left <= right) {
+
             // Top row
             for (int i = left; i <= right; i++) {
                 matrix[top][i] = num++;
             }
             top++;
+
             // Right column
             for (int i = top; i <= bottom; i++) {
                 matrix[i][right] = num++;
             }
             right--;
+
             // Bottom row
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
@@ -27,6 +40,7 @@ public class SpiralMatrix {
                 }
                 bottom--;
             }
+
             // Left column
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
@@ -35,26 +49,24 @@ public class SpiralMatrix {
                 left++;
             }
         }
+
         // Print matrix
-        System.out.println("Spiral Matrix:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + "\t");
+                System.out.print(matrix[i][j]);
+                if (j < n - 1) System.out.print(" ");
             }
             System.out.println();
         }
-        sc.close();
-    }
-}
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
 
+        // Calculate primary diagonal sum
+        int diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonalSum += matrix[i][i];
+        }
+
+        System.out.println("Diagonal: " + diagonalSum);
+
+        sc.close();
     }
 }
