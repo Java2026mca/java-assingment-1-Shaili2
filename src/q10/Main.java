8403f0c57960bf7cf77593ef2cfe573c4d6bfb34
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-08 00:23:54 +0530

    Added solution for q10

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -1,43 +1,53 @@
-import java.util.*;
+import java.util.Stack;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class PostfixEvaluation {
 
-        String line = sc.nextLine();
-        String[] tokens = line.split(" ");
-
+    public static int evaluatePostfix(String exp) {
         Stack<Integer> stack = new Stack<>();
+
+        // Split by space (important for multi-digit numbers)
+        String[] tokens = exp.split(" ");
 
         for (String token : tokens) {
 
-            // If operator
-            if (token.equals("+") || token.equals("-") || 
-                token.equals("*") || token.equals("/")) {
-
-                int b = stack.pop();
-                int a = stack.pop();
-
-                int result = 0;
-
-                switch (token) {
-                    case "+": result = a + b; break;
-                    case "-": result = a - b; break;
-                    case "*": result = a * b; break;
-                    case "/": result = a / b; break;
-                }
-
-                stack.push(result);
-            } 
-            // If operand
-            else {
+            // If number → push to stack
+            if (token.matches("-?\\d+")) {
                 stack.push(Integer.parseInt(token));
             }
+            // If operator → perform operation
+            else {
+                int val2 = stack.pop();
+                int val1 = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(val1 + val2);
+                        break;
+                    case "-":
+                        stack.push(val1 - val2);
+                        break;
+                    case "*":
+                        stack.push(val1 * val2);
+                        break;
+                    case "/":
+                        stack.push(val1 / val2);
+                        break;
+                    default:
+                        System.out.println("Invalid Operator");
+                        return 0;
+                }
+            }
         }
+        return stack.pop();
+    }
 
-        // Final result
-        System.out.println(stack.pop());
+    public static void main(String[] args) {
 
-        sc.close();
+        // IMPORTANT: space-separated postfix expression
+        String expression = "2 3 1 * + 9 -";
+
+        int result = evaluatePostfix(expression);
+
+        System.out.println("Result = " + result);
     }
 }
