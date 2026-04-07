5618d4957b37631a7c326e3e13f4b36839c40be2
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-07 23:18:35 +0530

    Added solution for Q3.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,11 +1,51 @@
-import java.util.*;
-
-public class Main {
+        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        import java.util.Scanner;
+public class SpiralMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
         int n = sc.nextInt();
-
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        int[][] matrix = new int[n][n];
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            // Bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            // Left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        // Print matrix
+        System.out.println("Spiral Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
         //       Print each row with values separated by single space
         //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
         //
