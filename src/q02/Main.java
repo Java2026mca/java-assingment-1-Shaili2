ed5481890a40ed5e65b82620a5b342c74b50529a
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-07 23:15:14 +0530

    Added solution Q2
    Implemented logic to print Pascal's Triangle for a given number of rows.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,11 +1,26 @@
-import java.util.*;
-
-public class Main {
+// TODO: Print Pascal's Triangle for n rows :
+import java.util.Scanner;
+public class PascalsTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows (n): ");
         int n = sc.nextInt();
-
-        // TODO: Print Pascal's Triangle for n rows
+        for (int i = 0; i < n; i++) {
+            // Print spaces for alignment
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            int num = 1;
+            // Print numbers
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
         //       Values on each row separated by single space
         //       No leading or trailing spaces
         //
