13847b69b4f7378fb0ccc6c6663112a06595334d
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-08 10:02:30 +0530

    Fix hollow diamond pattern printing in Main.java
    Refactor the diamond pattern printing logic to ensure proper spacing and structure.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,27 +1,42 @@
- // TODO: Print a hollow diamond pattern of half-height n using '*'
 
-       package q01;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
+
         for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) System.out.print(" ");
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
-                else System.out.print(" ");
+    
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
             }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
             System.out.println();
         }
+
         for (int i = n - 1; i >= 1; i--) {
-            for (int j = n; j > i; j--) System.out.print(" ");
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
-                else System.out.print(" ");
+    
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
             }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
             System.out.println();
         }
     }
