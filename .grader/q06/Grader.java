49d10f1bbc468e104b4a2c13c5c5723ca71d0ace
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-08 10:43:24 +0530

    Restore Grader.java - Fibonacci implementation

--- .grader/q06/Grader.java
+++ .grader/q06/Grader.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n <= 0) return;
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Print current Fibonacci number
+            System.out.print(a);
+
+            // Avoid trailing space
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+
+            // Generate next number
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        sc.close();
+    }
+}
