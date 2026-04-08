d244c78d3b0a669a30bd3f5220ba0fa2cbc05ae5
Shaili kushwaha <141387631+Shaili2@users.noreply.github.com>
2026-04-08 10:09:48 +0530

    Remove comments from matrix multiplication code
    Removed comments for input and output sections in matrix multiplication.

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -10,21 +10,18 @@ public class Main {
         int[][] B = new int[n][n];
         int[][] C = new int[n][n];
 
-        // Input matrix A
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
 
-        // Input matrix B
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
 
-        // Matrix multiplication C = A × B
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 C[i][j] = 0;
@@ -34,7 +31,6 @@ public class Main {
             }
         }
 
-        // Print result matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(C[i][j]);
